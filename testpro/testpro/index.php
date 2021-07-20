ac40e352c5d70e8fe69987dd6247436d92ea2679
freelanse <35501525+freelanse@users.noreply.github.com>
2021-07-20 23:29:15 +0300

    Add files via upload

---
+++ testpro/testpro/index.php
@@ -0,0 +1,288 @@
+<!DOCTYPE html>
+<html lang="en">
+
+	<head>
+		<!-- Meta -->
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+		
+		<title>Lorem Ipsum.</title>			
+	
+		<link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">		
+		<!-- Google Font -->
+		<link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700;800&display=swap" rel="stylesheet"> 
+		<link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;500;600;700;800;900&display=swap" rel="stylesheet"> 
+
+<link rel="stylesheet" href="assets/fonts/font-awesome.min.css">
+				<!-- Style CSS -->
+		<link rel="stylesheet" href="assets/css/style.css">	
+	
+	</head>
+	
+    <body data-spy="scroll" data-offset="80">
+
+		<!-- START PRELOADER -->
+		<div class="preloader">
+			<div class="status">
+				<div class="status-mes"></div>
+			</div>
+		</div>
+		<!-- END PRELOADER -->		
+
+		<!-- START NAVBAR -->  
+		<div id="navigation" class="fixed-top navbar-light bg-faded site-navigation">
+			<div class="container">
+				<div class="row">
+					<div class="col-lg-2 col-md-3 col-sm-4">
+						<div class="site-logo">
+							<a class="navbar-logo" href="index.html"><img src="assets/img/logo.png" alt=""></a>
+							<a class="navbar-logo" href="index.html"><img src="assets/img/logo-two.png" class="logo-hidden" alt=""></a>
+						</div>
+					</div><!--- END Col -->
+					
+					<div class="col-lg-10 col-md-9 col-sm-8">
+						<div class="header_right">
+							<nav id="main-menu" class="ml-auto">
+								<ul>
+								  <li><a href="#home">Home</a></li>
+								  <li><a href="#about">About Us</a></li>
+								  <li><a href="#software">Software</a></li>
+								  <li><a href="#pricing">Pricing</a></li>
+													  
+								  <a href="#contact" class="page-scroll btn btn-default btn-home-bg" style="padding:10px 30px; border-radius:20px;">Get Started</a>
+								</ul>
+							</nav>
+							<div id="mobile_menu"></div>
+						</div>
+					</div><!--- END Col -->
+				</div><!--- END ROW -->
+			</div><!--- END CONTAINER -->
+		</div> 	  
+		<!-- END NAVBAR -->
+		
+		<!-- START HOME -->
+		<section data-stellar-background-ratio="0.3" id="home" class="home_parallax" style="background-image: url(assets/img/bg/static-bg.png);  background-size:cover; background-position: center center;">			
+			<div class="container">
+				<div class="row">
+				  <div class="col-lg-12 text-center">
+					<div class="hero-text">
+						<h1>What is Lorem Ipsum?</h1>
+						<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+						<a href="#contact" class="page-scroll btn btn-default btn-home-bg">Button</a>						
+					</div>
+				  </div><!--- END COL -->		  
+				</div><!--- END ROW -->
+			</div><!--- END CONTAINER -->
+		</section>
+		<!-- END  HOME DESIGN -->
+
+		<!-- START ABOUT -->
+		<section id="about" class="about_us">
+					<div class="container">
+		       <div class="row">	
+		  <div class=" col-sm-4 " >
+									
+							<div class="count">
+							<div class="single_count">
+								<i><img src="assets/img/check-icon.png" width="50px" height="50px"></i>
+								<p>Contrary to popular belief, lorem Ipsum is not simply random text</p>
+							</div>		
+							
+						</div>
+					</div><!-- END COL -->			
+					  <div class=" col-sm-4 " >
+					
+						
+							<div class="count">
+							<div class="single_count">
+								<i><img src="assets/img/check-icon.png" width="50px" height="50px"></i>
+								<p>Contrary to popular belief, lorem Ipsum is not simply random text</p>
+							</div>		
+							
+						</div>
+					
+					</div>
+					
+					
+					<div class=" col-sm-4 ">
+					
+						
+							<div class="count">
+							<div class="single_count">
+								<i><img src="assets/img/check-icon.png" width="50px" height="50px"></i>
+								<p>Contrary to popular belief, lorem Ipsum is not simply random text</p>
+							</div>		
+							
+						</div>
+					</div><!-- END COL -->	
+					</div><!-- END COL -->		
+		</section>
+		<!-- END ABOUT -->	
+		
+		
+	<section id="software" class="our_team section-padding">
+<div class="container">
+<div class="col-md-12 text-center">
+<div class="section-title">
+<h1>Where does it come from?</h1>
+<p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using ’Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum’ will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).</p>
+</br>
+<h1>Where can I get some?</h1>
+<p>There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised 
+words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing
+</p>
+
+</div>
+</div>
+</div>
+</section>
+		<section id="pricing" class="our_service section-padding">
+			<div class="container">			
+				<div class="row">
+					
+	
+
+					<div class="col-md-12 text-center">
+						<div class="section-title" >
+							<h1>Discaunt's</h1>
+							
+						</div>
+					</div>
+				
+
+
+	<?php
+$a = array('Hosting1', 'Hosting2', 'Hosting3');
+$b = array ('XXXX Days Retention - U.S. + E.U. Servers', 'XXXX Days Retention - U.S. + E.U. Servers', 'XXXX Days Retention - U.S. + E.U. Servers');
+$c = array('$x.xx/mo for 1yr', '$x.xx/mo for 1yr', '$x.xx/mo for 1yr');
+$d = array('Unlimited downloads', 'Unlimited downloads 2', 'Unlimited downloads');
+$f = array('Unlimited speed', 'Unlimited speed', 'Unlimited speed');
+$g = array('All the Lorem Ipsum generators on the Internet tend to predefined chunks as necessary', 'All the Lorem Ipsum generators on the Internet tend to predefined chunks as necessary', 'All the Lorem Ipsum generators on the Internet tend to predefined chunks as necessary');
+$op = array('True Provider', 'True Provider', 'True Provider');
+for($i = 0; $i < count($a) & $i <count($b); $i++):
+ 
+?>
+
+				
+			<div class="container">	
+							
+							<img src="assets/img/bg/banner.png"  width="200px" height="40px" class="img-i">
+							
+						<div class="single_service" >
+												<div class='price'>				
+						<p class="img-p"><?php echo $op[$i] ?><p>
+							<h3 style="position: relative; top: -30px; left: -135px"><?php echo $a[$i] ?></h3> <p class="price-p"> | <?php echo $b[$i] ?></p>
+							</div>
+
+							<ul class="pric ">
+								<li><img src="assets/img/Price.png"  style="position: relative;top: -2px;left: -4px;"> <?php echo $c[$i] ?></li>  
+								<li class="price-li"><img src="assets/img/Downloads.png"   style="position: relative;top: -2px;left: -4px;"> <?php echo $d[$i] ?></li>
+								<li class="price-li"><img src="assets/img/Speed.png"  style="position: relative;top: -2px;left: -4px;">  <?php echo $f[$i] ?></li>
+							</ul>
+
+					<p style="position: relative;top: 20px;">
+					<img src="assets/img/check icon.png" width="30px" height="30px"  style="position: relative;top: -2px;left: -40px;"> <?php echo $g[$i] ?> </p>
+						
+					
+							  <a href="#contact" class="page-scroll btn btn-default btn-home-bg noc">Button</a>
+							  
+					</div>		
+						</div>
+					
+					  <br>
+		
+<?php endfor;?>
+</div>	
+
+
+
+				</div><!--- END ROW -->
+			</div><!--- END CONTAINER -->
+		</section>
+		<!-- END PROCESS -->
+		
+	
+		<!-- END PROJECTS -->
+		
+	
+		
+
+	
+	
+		<!-- START ADDRESS  -->
+		<section  class="our_team section-padding">
+			<div class="container">
+				<div class="row">
+				<div class="col-md-12 text-center">
+						
+					<h1>FAQ</h1></div></div>
+
+<main class="container">
+  <details open>
+
+				<summary>Why do we use it</summary>
+				<div class="faq__content">
+				<p> It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English</p>
+
+</div>
+  </details><hr>
+ <details>
+<summary>Where can I get some</summary>
+	 <div class="faq__content">
+				<p> There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing
+
+</p>
+</div>
+  </details><hr>
+ <details>
+<summary>Where does it come from?</summary>
+ <div class="faq__content">
+				<p> Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin 
+				literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney
+				 College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, 
+				 and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections
+
+</p>
+</details>
+   </main>
+					</div>
+				
+				</div><!--END  ROW  -->
+			</div><!-- END CONTAINER  -->
+		</section>
+		<!-- END ADDRESS -->
+		
+		<!-- FOOTER -->
+		<div class="footer">
+			<div class="container text-center">
+				<div class="row">
+					<div class="col-lg-12">
+						<div class="footer_copyright">
+							<p>&copy; 2021  Privacy Police. Terms of Service. Copyright Policy.</p>
+						</div>
+					</div>
+				</div>
+			</div><!--- END CONTAINER -->
+		</div>
+		<!-- END FOOTER -->		
+		 
+		<!-- Latest jQuery -->
+			<script src="assets/js/jquery-1.12.4.min.js"></script>
+		<!-- Latest compiled and minified Bootstrap -->
+			<script src="assets/bootstrap/js/bootstrap.min.js"></script>
+																	
+	
+		
+		<!-- jquery.slicknav -->
+			<script src="assets/js/jquery.slicknav.js"></script>	
+		<!-- jquery.smooth-scroll -->
+			<script src="assets/js/smooth-scroll.js"></script>			
+		<!-- magnific-popup js -->               
+		   
+			
+				
+		<!-- scripts js -->
+			<script src="assets/js/scripts.js"></script>
+    </body>
+</html>
